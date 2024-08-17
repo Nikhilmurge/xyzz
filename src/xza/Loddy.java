ba7556ef6b4ed3ce2d5099295a3445e56ea7afe7
fuck <nmurge123@gmail.com>
2024-08-17 19:08:45 +0530

    hello

---
+++ src/xza/Loddy.java
@@ -0,0 +1,15 @@
+package xza;
+
+public class Loddy {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+	public void m1() {
+			System.out.println("bolo");
+		}
+public void m2() {
+	System.out.println("cholo");
+	}
+
+}
