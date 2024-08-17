8189bbe1fa1a7d3ea6514edd06818f8546be4554
Nikhilmurge <nmurge123@gmail.com>
2024-08-17 19:13:03 +0530

    Update Loddy.java

--- src/xza/Loddy.java
+++ src/xza/Loddy.java
@@ -6,7 +6,7 @@ public class Loddy {
 		System.out.println("hello");
 	}
 	public void m1() {
-			System.out.println("bolo");
+	 System.out.println("bolo");
 		}
 public void m2() {
 	System.out.println("cholo");
